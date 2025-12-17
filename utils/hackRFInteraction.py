92d16d9dc4ccbc49797ff42ceac6a54c5023d3c2
Visti Brandt-Sørensen <visti@h031712.wifi.unknown.guest.rdns.aau.dk>
2025-12-17 20:37:01 +0100

    Modified RF for testing on pi again

---
+++ utils/hackRFInteraction.py
@@ -0,0 +1,58 @@
+import SoapySDR
+import numpy as np
+import math
+
+def readRssi():    
+    global sdr, rxStream, rssi
+    rssi_dbfs = None
+
+    # Read samples
+    buff = np.empty(4096, np.complex64)
+    for _ in range(50):
+        sr = sdr.readStream(rxStream, [buff], len(buff))
+        if sr.ret <= 0:
+            continue
+        
+        x = buff[:sr.ret]
+        power = float(np.mean((x.real * x.real) + (x.imag * x.imag)))
+
+        if not math.isfinite(power) or power <= 0.0:
+            continue
+    
+
+        rssi_dbfs = 10.0 * math.log10(power + 1e-12)
+        rssi = rssi_dbfs  # store globally
+    
+    return rssi_dbfs
+
+
+def setupHackRF():
+    global sdr
+    global rxStream
+    devices = SoapySDR.Device.enumerate()
+    sdr = SoapySDR.Device(devices[0])  # Assuming the first device is the HackRF
+    sdr.setSampleRate(SoapySDR.SOAPY_SDR_RX, 0, 10e6)
+    sdr.setFrequency(SoapySDR.SOAPY_SDR_RX, 0, channels[0]*1e6)
+    sdr.setGain(SoapySDR.SOAPY_SDR_RX, 0, 20)
+    # Setup stream
+    rxStream = sdr.setupStream(SoapySDR.SOAPY_SDR_RX, SoapySDR.SOAPY_SDR_CF32)
+    sdr.activateStream(rxStream)
+
+def closeHackRF():
+    global sdr, rxStream
+    try:
+        if rxStream is not None:
+            sdr.deactivateStream(rxStream)
+            sdr.closeStream(rxStream)
+    except Exception:
+        pass
+    rxStream = None
+    sdr = None
+
+def main():
+    setupHackRF()
+    readRssi()
+    closeHackRF()
+
+if __name__ == "__main__":
+    main()
--- utils/rfUtils.py
+++ utils/rfUtils.py
@@ -7,67 +7,24 @@ import utils.modes as modes
 from utils.config import *
 import utils.motorUtils as motorUtils
 import threading
-
+import subprocess
 
 # Global variable to keep track of the current scan section
 baseline_avgs = {}   # Baseline averages for the 101 sections
-sdr = None  # Global SDR object
-rxStream = None  # Global RX stream object
-rssi = None
-sdr_lock = threading.Lock()
+rssi = None  # Global variable to store the latest RSSI value
 
-
-def setupHackRF():
-    global sdr
-    global rxStream
-    devices = SoapySDR.Device.enumerate()
-    sdr = SoapySDR.Device(devices[0])  # Assuming the first device is the HackRF
-    sdr.setSampleRate(SoapySDR.SOAPY_SDR_RX, 0, 10e6)
-    sdr.setFrequency(SoapySDR.SOAPY_SDR_RX, 0, channels[0]*1e6)
-    sdr.setGain(SoapySDR.SOAPY_SDR_RX, 0, 20)
-    # Setup stream
-    rxStream = sdr.setupStream(SoapySDR.SOAPY_SDR_RX, SoapySDR.SOAPY_SDR_CF32)
-    sdr.activateStream(rxStream)
-
-def closeHackRF():
-    global sdr, rxStream
-    try:
-        if rxStream is not None:
-            sdr.deactivateStream(rxStream)
-            sdr.closeStream(rxStream)
-    except Exception:
-        pass
-    rxStream = None
-    sdr = None
 
 
 def readRssi():
-    global sdr, rxStream, rssi
-    setupHackRF()
-    rssi_dbfs = None
+    global rssi
+    result = subprocess.run(
+    ["python3", "hackRFInteraction.py"],
+    capture_output=True,
+    text=True
+)
 
-    # Read samples
-    buff = np.empty(4096, np.complex64)
-    for _ in range(50):
-        sr = sdr.readStream(rxStream, [buff], len(buff))
-        if sr.ret <= 0:
-            continue
-        
-        x = buff[:sr.ret]
-        power = float(np.mean((x.real * x.real) + (x.imag * x.imag)))
-
-        if not math.isfinite(power) or power <= 0.0:
-            continue
-    
-
-        rssi_dbfs = 10.0 * math.log10(power + 1e-12)
-        rssi = rssi_dbfs  # store globally
-    
-
-        print(power)
-        print(f"Estimated RSSI: {rssi_dbfs:.2f} dBFS")
-    closeHackRF()
-    return rssi_dbfs
+    rssi = float(result.stdout)
+    return rssi
 
 
 def getMaxRssi():
